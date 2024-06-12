3245a63ff11c7c66a2eb9006a834a818cf9861e1
seoyeon83 <mool848777@naver.com>
2024-06-12 16:34:15 +0900

    feat(dag): naver flight dag update

--- temp_dag/get_price_flight.py
+++ temp_dag/get_price_flight.py
@@ -4,6 +4,7 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.decorators import task
 import psycopg2
 
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service   #드라이버는 브라우저마다 다르다
 from webdriver_manager.chrome import ChromeDriverManager    # pc에 설치된 크롬과 버전을 같게 하기 위해
@@ -30,11 +31,23 @@ def get_Redshift_connection(autocommit=True):
     return conn.cursor()
 
 @task
-def get_naver_flight_price(departure_date, arrival_date, departure_airport, arrival_airport, roundtrip = True):
+def get_naver_flight_price(departure_date, departure_airport, arrival_date, arrival_airport, roundtrip = True):
+    print(departure_date, type(departure_date), arrival_date, type(arrival_date))
+    departure_date = departure_date.strftime('%Y%m%d')
+    arrival_date = arrival_date.strftime('%Y%m%d')
+
     logging.info(datetime.utcnow())
     logging.info(f"출발일: {departure_date}, 출발지: {departure_airport}, 도착일: {arrival_date}, 도착지: {arrival_airport}, 왕복 여부: {roundtrip}")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.4.1.6 Safari/537.36"
+    options.add_argument('user-agent=' + user_agent)
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     if roundtrip:
         url = f"https://flight.naver.com/flights/international/{departure_airport}-{arrival_airport}-{departure_date}/"\
@@ -122,51 +135,104 @@ def get_naver_flight_price(departure_date, arrival_date, departure_airport, arri
             departure_landing_time = route_times[1].get_text(strip=True)
 
             logging.info(f"출발시 이륙 시간 : {departure_takeoff_time}, 출발시 착륙 시간: {departure_landing_time}")
+
+            # 편도일 경우 Null 값이 되는 컬럼
+            arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time = None, None, None, None, None
+
     except:
         pass
         # raise
     finally:
         driver.quit()
     
-    # return [name, itemID , price]
+    return [price, departure_airline, departure_layover_count, departure_duration, departure_takeoff_time, departure_landing_time,\
+            arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time]
 
+
+# @task
+# def create_table(schema, iteminfo):
+#     logging.info("create table started")
+#     name, itemID, price = iteminfo    
+#     cur = get_Redshift_connection()   
+#     try:
+#         cur.execute("BEGIN;")
+#         cur.execute(f"CREATE TABLE IF NOT EXISTS {schema}.{name}{itemID} (datetime timestamp, price int);")
+#         cur.execute("COMMIT;") 
+#         logging.info(f"created {name}{itemID} table done")
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#         cur.execute("ROLLBACK;")   
+
+def convert_to_sql_interval(time_str):
+    # 정규 표현식을 사용하여 시간과 분을 추출
+    pattern = r'(\d+)\s*시간\s*(\d+)\s*분'
+    match = re.match(pattern, time_str)
+    if match:
+        hours = int(match.group(1))
+        minutes = int(match.group(2))
+        # SQL INTERVAL 형식으로 변환하여 반환
+        return f"INTERVAL '{hours}:{minutes:02}' HOUR TO MINUTE"
+    else:
+        # 일치하는 패턴이 없을 경우 None 반환
+        return None
+    
+@task
+def transform(data):
+    price, departure_airline, departure_layover_count, departure_duration, departure_takeoff_time, departure_landing_time,\
+            arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time = data
+    
+    # 소요시간 INTERVAL type으로
+    departure_duration = convert_to_sql_interval(departure_duration)
+    if arrival_duration:
+        arrival_duration = convert_to_sql_interval(arrival_duration)
+
+    # 이륙, 착륙 시간 00:00:00 형식으로
+    departure_takeoff_time = f"{departure_takeoff_time}:00"
+    departure_landing_time = f"{departure_landing_time}:00"
+    if arrival_takeoff_time:
+        arrival_takeoff_time = f"{arrival_takeoff_time}:00"
+    if arrival_landing_time:
+        arrival_landing_time = f"{arrival_landing_time}:00"
+
+    return [price, departure_airline, departure_layover_count, departure_duration, departure_takeoff_time, departure_landing_time,\
+            arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time]
 
 @task
-def create_table(schema, iteminfo):
-    logging.info("create table started")
-    name, itemID, price = iteminfo    
-    cur = get_Redshift_connection()   
-    try:
-        cur.execute("BEGIN;")
-        cur.execute(f"CREATE TABLE IF NOT EXISTS {schema}.{name}{itemID} (datetime timestamp,price int);")
-        cur.execute("COMMIT;") 
-        logging.info(f"created {name}{itemID} table done")
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        cur.execute("ROLLBACK;")   
-
-
-@task
-def load(schema, iteminfo):
+def load(schema, tablename, id, data):
     logging.info("load started")
-    name, itemID, price = iteminfo    
+    datetime.utcnow()
+    price, departure_airline, departure_layover_count, departure_duration, departure_takeoff_time, departure_landing_time,\
+            arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time = data
     cur = get_Redshift_connection()   
 
     try:
         cur.execute("BEGIN;")
-        sql = f"INSERT INTO {schema}.{name}{itemID} VALUES ('{datetime.utcnow()}', '{price}')"
-        cur.execute(sql)
+        sql = f"""
+            INSERT INTO {schema}.{tablename} (
+                id, ts, price, departure_airline, departure_layover_count, departure_duration, departure_takeoff_time, departure_landing_time,
+                arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time
+            ) VALUES (
+                %s, GETUTCDATE(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+            )
+        """
+        cur.execute(sql, (
+            id, price, departure_airline, departure_layover_count, departure_duration, departure_takeoff_time, departure_landing_time,
+            arrival_layover_count, arrival_airline, arrival_duration, arrival_takeoff_time, arrival_landing_time
+        ))
         cur.execute("COMMIT;") 
+
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        cur.execute("ROLLBACK;")   
+        logging.error(error)
+        cur.execute("ROLLBACK;")
+        raise
+
     logging.info("load done")
 
 
 def get_flight_info():
     cursor = get_Redshift_connection()  
 
-    cursor.execute('SELECT * FROM table_name')  # 테이블 이름 설정해야함
+    cursor.execute('SELECT * FROM mool8487.flight_ticket_price_info')  # 테이블 이름 설정해야함
     records = cursor.fetchall()
 
     cursor.close()
@@ -187,12 +253,13 @@ with DAG(
     }
 ) as dag:
     schema = 'mool8487'   ## 자신의 스키마로 변경
+    tablename = 'flight_ticket_price_history'
 
     records = get_flight_info()
 
     for record in records:
-        # departure_date, departure_airflow, arrival_date, arrival_airport
-        data = get_naver_flight_price(record)
-
-        create_table(schema, data)
-        load(schema, data)
+        print(record)
+        id, departure_date, departure_airport, arrival_date, arrival_airport, is_roundtrip = record
+        
+        data = transform(get_naver_flight_price(departure_date, departure_airport, arrival_date, arrival_airport, is_roundtrip))
+        load(schema, tablename, id, data)
