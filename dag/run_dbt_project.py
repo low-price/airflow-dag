e82563912b88d59a5dc83c5d8af191878ad6aa64
juyounglee <128959957+ljylyb289@users.noreply.github.com>
2024-06-13 17:02:35 +0900

    feat: make dag for connect to dbt server

---
+++ dag/run_dbt_project.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'start_date': datetime(2022, 1, 1),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
+}
+
+with DAG(
+    'run_dbt_project',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+) as dag:
+
+    run_dbt_project = SSHOperator(
+        task_id='run_dbt_project',
+        ssh_conn_id='test_ssh_conn',
+        command='cd ~ && dbt test && dbt run',
+        dag=dag
+    )
+
+    run_dbt_project
