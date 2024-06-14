741f9578440184fb5f8f901c13d2b86d2b0353de
seoyeon83 <mool848777@naver.com>
2024-06-14 15:25:51 +0900

    feat(dag): 구글 스프레드 시트에서 dataframe으로 가져오도록

--- temp_dag/load_flight_ticket_info.py
+++ temp_dag/load_flight_ticket_info.py
@@ -1,16 +1,14 @@
 from airflow import DAG
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 import psycopg2
 from airflow.models import Variable
 
 from datetime import datetime
 from datetime import timedelta
 import logging
-
-from plugins import gsheet
-from plugins import s3
+import gspread
+import pandas as pd
 
 
 def get_Redshift_connection(autocommit=True):
@@ -20,71 +18,83 @@ def get_Redshift_connection(autocommit=True):
     return conn.cursor()
 
 
+import gspread
+import pandas as pd
+import json
+from google.oauth2.service_account import Credentials
+from airflow.models import Variable
 
-def download_tab_in_gsheet(**context):
+
+def read_tab_in_gsheet_to_df(**context):
     url = context["params"]["url"]
     tab = context["params"]["tab"]
-    table = context["params"]["table"]
-    data_dir = Variable.get("DATA_DIR")
 
-    gsheet.get_google_sheet_to_csv(
-        url,
-        tab,
-        data_dir+'{}.csv'.format(table)
-    )
+    # Google Sheets API credentials
+    creds_json = Variable.get("google_sheet_access_token")
+    creds_dict = json.loads(creds_json)
+    
+    # Set the correct scope for Google Sheets API
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 
+    # Initialize the Google Sheets client
+    client = gspread.authorize(creds)
 
+    # Open the Google Sheet by URL
+    sheet = client.open_by_url(url)
 
-def copy_to_s3(**context):
-    table = context["params"]["table"]
-    s3_key = context["params"]["s3_key"]
+    # Select the specified tab
+    worksheet = sheet.worksheet(tab)
 
-    s3_conn_id = "aws_conn_id"
-    s3_bucket = "grepp-data-engineering"
-    data_dir = Variable.get("DATA_DIR")
-    local_files_to_upload = [ data_dir+'{}.csv'.format(table) ]
-    replace = True
+    # Get all values in the worksheet
+    data = worksheet.get_all_values()
 
-    s3.upload_to_s3(s3_conn_id, s3_bucket, s3_key, local_files_to_upload, replace)
+    # Convert to DataFrame
+    df = pd.DataFrame(data[1:], columns=data[0])
 
+    return df
 
 
 # info temp 테이블 및 info 테이블 함께 조회하며 중복되지 않는 record 찾기
 # 두 테이블 조인해서 id가 null인 행 추출
 def check_new_ticket_info(**context):
     schema = context["params"]["schema"]
-    infotable = context["params"]["infotable"]
-    temptable = context["params"]["temptable"]
+    table = context["params"]["table"]
+    df = context["task_instance"].xcom_pull(key="return_value", task_ids='read_tab_in_gsheet_to_df')
 
     logging.info("check new ticket info start")
 
     cursor = get_Redshift_connection()
-    sql = f"""
-        SELECT t.departure_date, t.departure_airport, t.arrival_date, t.arrival_airport, t.is_roundtrip
-        FROM {schema}.{infotable} i
-        FULL OUTER JOIN {schema}.{temptable} t
-        ON 
-            i.departure_date = t.departure_date
-            AND i.departure_airport = t.departure_airport
-            AND i.arrival_date = t.arrival_date
-            AND i.arrival_airport = t.arrival_airport
-            AND i.is_roundtrip = t.is_roundtrip
-        WHERE i.id IS NULL;
-    """
-    cursor.execute(sql)
-    records = cursor.fetchall()
+
+    new_tickets = []
+    count = 0
+    for _, row in df.iterrows():
+        cursor.execute("BEGIN;")
+        sql = f"""
+                SELECT COUNT(*) FROM {schema}.{table} 
+                WHERE departure_date=%s AND departure_airport=%s AND arrival_date=%s 
+                AND arrival_airport=%s AND is_roundtrip=%s;"""
+        
+        cursor.execute(sql, (
+            row["departure_date"], row["departure_airport"], row["arrival_date"], row["arrival_airport"], row["is_roundtrip"]
+        ))
+        new = cursor.fetchone()[0]
+        if new == 0:
+            logging.info(list(row))
+            new_tickets.append(list(row))
+            count += 1
 
     cursor.close()
 
     logging.info(f"check new ticket_info done")
 
-    return records
+    return new_tickets
 
 
 def load(**context):
     schema = context["params"]["schema"]
-    table = context["params"]["infotable"]
-    records = context['task_instance'].xcom_pull(key="return_value", task_ids='check_new_ticket_info')
+    table = context["params"]["table"]
+    new_tickets = context['task_instance'].xcom_pull(key="return_value", task_ids='check_new_ticket_info')
 
     logging.info("load started")
 
@@ -94,7 +104,7 @@ def load(**context):
     next_id = cursor.fetchone()[0]
     logging.info(f"next_id : {next_id}")
 
-    for record in records:
+    for record in new_tickets:
         departure_date, departure_airport, arrival_date, arrival_airport, is_roundtrip = record
         
         try:
@@ -125,7 +135,7 @@ def load(**context):
 
 
 dag = DAG(
-    dag_id='load_flight_ticket_info',
+    dag_id='load_flight_ticket_info_v2',
     start_date=datetime(2024, 6, 1),  
     schedule='@once', #'*/10 * * * *',  
     max_active_runs=1,
@@ -136,68 +146,33 @@ dag = DAG(
     }
 )
 
-file_path = "naver_flight_ticket_info.csv"
-schema = 'mool8487' #'jheon735'  
-infotable = 'flight_ticket_info'
-temptable = 'flight_ticket_info_temp'
 
-# 추후 논의 후 시트 정보 수정
 sheet = {
         "url": "https://docs.google.com/spreadsheets/d/1AJICer9RRJ-G3zxAc9Lk73gfK-nM0yn4gTRBK1Kbkzg/", 
         "tab": "FlightTicketInfo",
-        "schema": "mool8487",
-        "table": "flight_ticket_info_temp"
+        "schema": "mool8487", #'jheon735'  
+        "table": "flight_ticket_info"
 }
 
-download_tab_in_gsheet = PythonOperator(
-        task_id = 'download_tab_in_gsheet',
-        python_callable = download_tab_in_gsheet,
+read_tab_in_gsheet_to_df = PythonOperator(
+        task_id = 'read_tab_in_gsheet_to_df',
+        python_callable = read_tab_in_gsheet_to_df,
         params = sheet,
         dag = dag)
-
-s3_key = sheet["schema"] + "_" + sheet["table"]
-
-copy_to_s3 = PythonOperator(
-    task_id = 'copy_to_s3',
-    python_callable = copy_to_s3,
-    params = {
-        "table": sheet["table"],
-        "s3_key": s3_key
-    },
-    dag = dag)
-
-run_copy_sql = S3ToRedshiftOperator(
-    task_id = 'run_copy_sql',
-    s3_bucket = "grepp-data-engineering",
-    s3_key = s3_key,
-    schema = sheet["schema"],
-    table = sheet["table"],
-    copy_options = ['csv', 'IGNOREHEADER 1'],
-    method = 'REPLACE',
-    redshift_conn_id = "redshift_dev_db",
-    aws_conn_id = 'aws_conn_id',
-    dag = dag
-)
 
 check_new_ticket_info = PythonOperator(
     task_id = 'check_new_ticket_info',
     python_callable = check_new_ticket_info,
-    params = {
-        "schema" : schema,
-        "infotable" : infotable,
-        "temptable" : temptable
-    },
+    params = sheet,
     dag = dag
 )
 
 load = PythonOperator(
     task_id='product_info_load',
     python_callable=load,
-    params = {
-        "schema" : schema,
-        "infotable" : infotable
-    },
+    params = sheet,
     dag = dag
     )
 
-download_tab_in_gsheet >> copy_to_s3 >> run_copy_sql >> check_new_ticket_info >> load
+
+read_tab_in_gsheet_to_df >> check_new_ticket_info >> load
